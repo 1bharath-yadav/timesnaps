4ce9ce27b3e27a080f0ee34e281bb600b7cc5bde
1bharath_yadav <byadhav36@gmail.com>
2024-10-18 17:36:44 +0530

    commit

--- pkg/timesnaps/usr/bin/timesnap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-screenshot_dir="/home/$(whoami)/timesnaps"
-
-if [ ! -d "$screenshot_dir" ]; then
-    mkdir -p "$screenshot_dir"
-fi
-
-spectacle -f -b -n -o "$screenshot_dir/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png"
--- src/timesnap.sh
+++
@@ -1,1 +0,0 @@
-/home/Archer/projects/timesnaps/timesnap.sh
